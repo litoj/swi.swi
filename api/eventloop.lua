76baff261306aed28f660ede12a64f2d2b3ef251
litoj <invisiblemancz@gmail.com>
2026-04-05 13:42:03 +0200

    ref(eventloop): index by pattern instead of by mode

--- api/eventloop.lua
+++ api/eventloop.lua
@@ -8,13 +8,15 @@ local tabled = U.tabled
 ---@field pattern table<string|integer,string>
 ---@field mode string[]
 
----@type swi.eventloop
+---@class swi.api.eventloop: swi.eventloop
 local M = {
-	---@type {[event_name_t]:{[appmode_t]:{[hook_id]:swi.eventloop.hook}}}
+	---@type {[event_name_t]:{[string]:swi.eventloop.hook[]}}
 	_hooks = {},
 	debug_trigger = false,
 	debug_subscribe = false,
 }
+
+local modes = { 'viewer', 'gallery', 'slideshow' }
 
 local function print_debug(name, t)
 	if t.event == 'Subscribed' and name == 'trigger' then return end
@@ -25,7 +27,7 @@ end
 ---@param cfg swi.eventloop.subscribe.opts
 ---@return swi.eventloop.hook
 local function mk_hook(cfg)
-	local t = tabled(cfg.pattern or {})
+	local t = tabled(cfg.pattern or { '.' })
 	local i = #t
 	while i > 0 do
 		local p = t[i]
@@ -41,62 +43,117 @@ local function mk_hook(cfg)
 		i = i - 1
 	end
 	cfg.pattern = t ---@cast cfg swi.eventloop.hook
+	cfg.mode = U.rev_idx(tabled(cfg.mode or modes))
 	return cfg
 end
 
-local function has_match(match, ptnlist)
-	if not match or not next(ptnlist) then return true end
-	local direct = ptnlist[match]
-	if direct ~= nil then return direct end
-	for _, p in ipairs(ptnlist) do
-		if match:match(p) then return true end
+function M.subscribe(hook)
+	if not hook.callback then error('missing callback in: ' .. tostring(hook)) end
+	if M.debug_subscribe then print_debug('subscribe', hook) end
+	hook = mk_hook(hook)
+	for _, e in ipairs(tabled(hook.event or error('missing event in: ' .. tostring(hook)))) do
+		local ev_hooks = M._hooks[e]
+		if not ev_hooks then
+			ev_hooks = {}
+			M._hooks[e] = ev_hooks
+		end
+
+		for k, v in pairs(hook.pattern) do
+			if v then
+				k = type(k) == 'string' and k or '*'
+				---@diagnostic disable-next-line: cast-local-type
+				v = ev_hooks[k]
+				if not v then ---@diagnostic disable-next-line: cast-local-type
+					v = {}
+					ev_hooks[k] = v
+				end
+				v[#v + 1] = hook
+			end
+		end
+
+		M.trigger { event = 'Subscribed', mode = hook.mode, match = e, data = hook }
 	end
+
+	return hook
 end
 
-local modes = { 'viewer', 'gallery', 'slideshow' }
----@param f swi.eventloop.filter.opts
----@param on_match fun(h:swi.eventloop.hook,idx:hook_id,m:appmode_t,ev:event_name_t)
-local function apply_filtered(f, on_match)
-	local modes = tabled(f.mode or modes)
-	for _, ev in pairs(tabled(f.event or U.rev_idx(M._hooks))) do
-		local ev_hooks = M._hooks[ev]
-		if ev_hooks then
-			for _, m in pairs(modes) do
-				local m_hooks = ev_hooks[m]
-				if m_hooks then
-					for i, hook in pairs(m_hooks) do
-						local ok = has_match(f.match, hook.pattern)
-						if f.id and ok then ok = f.id == i end
-						if f.group and ok then ok = f.group == hook.group end
-						if ok then on_match(hook, i, m, ev) end
+---@param ptn_map {[string]:swi.eventloop.hook[]}
+---@return fun():(hook:swi.eventloop.hook?,ptn:string,i:integer)
+local function matcher(match, ptn_map)
+	return coroutine.wrap(function()
+		if not match then
+			for ptn, hooks in pairs(ptn_map) do
+				for i, h in pairs(hooks) do
+					coroutine.yield(h, ptn, i)
+				end
+			end
+			return
+		end
+
+		local hooks = ptn_map[match]
+		if hooks then
+			for i, h in pairs(hooks) do
+				coroutine.yield(h, match, i)
+			end
+		end
+
+		hooks = ptn_map['*']
+		if hooks then
+			for i, h in ipairs(hooks) do
+				if h.pattern[match] == nil then -- `true` was already processed, `false` is to skip it
+					for _, ptn in pairs(h.pattern) do
+						if match:match(ptn) then
+							coroutine.yield(h, '*', i)
+							break
+						end
 					end
 				end
 			end
 		end
-	end
-end
-
-function M.unsubscribe(f)
-	apply_filtered(f, function(_, id, m, ev)
-		local ev_hooks = M._hooks[ev]
-		local m_hooks = ev_hooks[m]
-		local m_idx = next(m_hooks)
-		if m_idx == id and not next(m_hooks, m_idx) then
-			local ev_idx = next(ev_hooks)
-			if ev_idx == m and not next(ev_hooks, ev_idx) then
-				M._hooks[ev] = nil
-			else
-				ev_hooks[m] = nil
-			end
-		else
-			m_hooks[id] = nil
-		end
 	end)
 end
 
+---@alias swi.eventloop.applicator fun(h:swi.eventloop.hook,ev:event_name_t, pnt:string,i:integer)
+
+---@param f swi.eventloop.filter.opts
+---@param on_match swi.eventloop.applicator
+function M.apply_filtered(f, on_match)
+	for _, ev in pairs(tabled(f.event or U.rev_idx(M._hooks))) do
+		local ev_hooks = M._hooks[ev]
+		if ev_hooks then
+			f.mode = tabled(f.mode or modes)
+			for hook, ptn, i in matcher(f.match, ev_hooks) do
+				local ok
+				for _, m in pairs(modes) do
+					ok = hook.mode[m]
+					if ok then break end
+				end
+				if ok and f.id then ok = f.id == hook end
+				if ok and f.group then ok = f.group == hook.group end
+				if ok then on_match(hook, ev, ptn, i) end
+			end
+		end
+	end
+end
+
+---@type swi.eventloop.applicator
+local function raw_unsub(hook, ev, ptn, i)
+	local ev_hooks = M._hooks[ev]
+	local ptn_hooks = ev_hooks[ptn]
+
+	ptn_hooks[i] = nil
+
+	if not next(ptn_hooks) then
+		ev_hooks[ptn] = nil
+		if not next(ev_hooks) then M._hooks[ev] = nil end
+	end
+end
+
+function M.unsubscribe(f) M.apply_filtered(f, raw_unsub) end
+
 function M.get_subscribed(f)
 	local t = {}
-	apply_filtered(f or {}, function(h, id) t[id] = h end)
+	M.apply_filtered(f or {}, function(h) t[h] = h end)
 	return t
 end
 
@@ -105,43 +162,16 @@ function M.trigger(state)
 
 	---@cast state swi.eventloop.filter.opts
 	state.mode = state.mode or swayimg.get_mode()
-	apply_filtered(state, function(hook)
+	M.apply_filtered(state, function(hook, ...)
 		local ok, ret = xpcall(hook.callback, debug.traceback, state)
 		if not ok then
 			---@diagnostic disable-next-line: param-type-mismatch
 			swayimg.text.set_status(string.gsub(ret, '\t', '  '))
 			print(ret)
 		elseif ret then
-			M.unsubscribe { id = hook }
+			raw_unsub(hook, ...)
 		end
 	end)
-end
-
-function M.subscribe(hook) -- TODO: generalize ptn matching to matching and registering mode
-	if not hook.callback then error('missing callback in: ' .. tostring(hook)) end
-	if M.debug_subscribe then print_debug('subscribe', hook) end
-	hook = mk_hook(hook)
-	hook.mode = tabled(hook.mode or modes)
-	for _, e in ipairs(tabled(hook.event or error('missing event in: ' .. tostring(hook)))) do
-		local ev_hooks = M._hooks[e]
-		if not ev_hooks then
-			ev_hooks = {}
-			M._hooks[e] = ev_hooks
-		end
-
-		for _, m in ipairs(hook.mode) do
-			local m_hooks = ev_hooks[m]
-			if not m_hooks then
-				m_hooks = {}
-				ev_hooks[m] = m_hooks
-			end
-
-			m_hooks[hook] = hook
-		end
-		M.trigger { event = 'Subscribed', mode = hook.mode, match = e, data = hook }
-	end
-
-	return hook
 end
 
 swayimg.on_initialized(function()
